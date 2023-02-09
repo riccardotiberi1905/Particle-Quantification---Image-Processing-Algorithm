88e3f939f585ccc59219ad71002056480ca407e3
riccardotiberi1905 <109066306+riccardotiberi1905@users.noreply.github.com>
2023-02-09 12:10:07 +0100

    Update table_characteristics.m

--- table_characteristics.m
+++ table_characteristics.m
@@ -1,45 +1,67 @@
-function [stats_bin, NumTrombosActualizado, stats_below_500, stats500, stats800, stats1000, Circumference] = table_characteristics(BIN1, BW11)
-%Initial number of thrombi detected by the algorithm
-label = bwlabel(BW11);
-label_bin = bwlabel(BIN1);
-NumTrombos = max(max(label));
-NumTrombos_bin = max(max(label_bin));
-bw = bwareafilt(BW11,NumTrombos);
-bw_bin = bwareafilt(BIN1,NumTrombos_bin);
+function [stats_roi, n_thrombi, stats_below_500, stats_500, stats_800, stats_1000, major_axis_length] = table_characteristics(negative_binary_image, negative_roi_binary_image)
+    %{
+        Function description.
 
-%Table that shows the major- and minor axis length + area for each element
-stats = sortrows(regionprops('table',bw, 'MajorAxisLength','MinorAxisLength', 'Area'));
-stats_bin = sortrows(regionprops('table',bw_bin, 'MajorAxisLength','MinorAxisLength', 'Area'));
-LongLinea = (max(stats.MajorAxisLength) + max(stats.MinorAxisLength))/2;
-Circumference = max(stats.MajorAxisLength);
+        Parameters
+        ----------
+        negative_binary_image : single channel uint8 matrix
+            Complementary binary image of original image.
+        negative_roi_binary_image : single channel uint8 matrix
+            Complementary binary image of cropped image.
 
-%Conversion pixel - 1um
-Conversion = (.8*10^4/LongLinea);
+        Returns
+        -------
+        stats_roi : Table with all detected particles and their features
+            according to Major Axis Length (MAL), Minor Axis Length (MIL) and
+            Area.
+        n_thrombi : Number of thrombi in the image.
+        stats_below_500 : Table for all thrombi with MAL < 500um
+        stats_500 : Table for all thrombi with MAL > 500um
+        stats_800 : Table for all thrombi with MAL > 800um
+        stats_1000 : Table for all thrombi with MAL > 1000um
+        major_axis_length : Length of the largest detected particle in um.
+    %}
+    % Initial number of thrombi detected by the algorithm (complete image)
+    label = bwlabel(negative_binary_image);
+    n_thrombi = max(max(label));
+    label_filt = bwareafilt(negative_binary_image, n_thrombi);
+    % Initial number of thrombi detected by the algorithm (roi)
+    label_roi = bwlabel(negative_roi_binary_image);
+    n_thrombi_roi = max(max(label_roi));
+    label_filt_roi = bwareafilt(negative_roi_binary_image, n_thrombi_roi);
 
-%We convert area, MIL and MAL from pixel units to um units
-for i=1:NumTrombos_bin
-    stats_bin.MajorAxisLength(i) = stats_bin.MajorAxisLength(i)*Conversion;
-    stats_bin.MinorAxisLength(i) = stats_bin.MinorAxisLength(i)*Conversion;
-    stats_bin.Area(i) = stats_bin.Area(i)*(Conversion)^2;
+    % Table that shows the major axis length (MIL), minor axis length (MAL) and area for each element of the image
+    stats = sortrows(regionprops('table', label_filt, 'MajorAxisLength', 'MinorAxisLength', 'Area'));
+    mean_axis_length = (max(stats.MajorAxisLength) + max(stats.MinorAxisLength)) / 2;
+    major_axis_length = max(stats.MajorAxisLength);
+
+    % Conversion pixel to um
+    conversion = (0.8 * 10 ^ 4 / mean_axis_length);
+
+    % Table that shows the MIL, MAL and area for each element of the cropped image
+    stats_roi = sortrows(regionprops('table', label_filt_roi, 'MajorAxisLength', 'MinorAxisLength', 'Area'));
+
+    % We convert MIL, MAL and area from pixel units to um units
+    for i = 1:n_thrombi_roi
+        stats_roi.MajorAxisLength(i) = stats_roi.MajorAxisLength(i) * conversion;
+        stats_roi.MinorAxisLength(i) = stats_roi.MinorAxisLength(i) * conversion;
+        stats_roi.Area(i) = stats_roi.Area(i)*(conversion)^2;
+    end
+
+    % We remove all thrombi with a MAL < 100 um
+    particles_to_delete = stats_roi.MajorAxisLength < 100;
+    stats_roi(particles_to_delete, :) = [];
+
+    % Update thrombi count
+    n_thrombi = height(stats_roi);
+
+    % We create another table with only values for MAL < 500um
+    stats_below_500 = stats_roi(stats_roi.MajorAxisLength < 500, :);
+    % We create another table with only values for MAL > 500um
+    stats_500 = stats_roi(stats_roi.MajorAxisLength > 500, :);
+    % We create another table with only values for MAL > 800um
+    stats_800 = stats_roi(stats_roi.MajorAxisLength > 800, :);
+    % We create another table with only values for MAL > 1000um
+    stats_1000 = stats_roi(stats_roi.MajorAxisLength > 1000, :);
+
 end
-
-%We eliminate each thrombi with a major axis length <100um
-toDelete = stats_bin.MajorAxisLength < 70; %Elimina todos los trombos menores a 100 micras
-stats_bin(toDelete,:) = [];
-
-%The following table shows the updated number of thrombi
-NumTrombosActualizado = height(stats_bin);
-
-%We create another table with only values for MAL >500um
-stats_below_500 = stats_bin(stats_bin.MajorAxisLength < 500,:);
-
-%We create another table with only values for MAL >500um
-stats500 = stats_bin(stats_bin.MajorAxisLength > 500,:);
-
-%We create another table with only values for MAL >800um
-stats800 = stats_bin(stats_bin.MajorAxisLength > 800,:);
-
-%We create another table with only values for MAL >1000um
-stats1000 = stats_bin(stats_bin.MajorAxisLength > 1000,:);
-end
-
